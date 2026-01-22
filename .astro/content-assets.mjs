004308f90532b835c259d2394b8381411ca882af
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2026-01-21 17:01:20 -0800

    removing .astro directory

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_3iL3t from "/src/images/blog/hadouken.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Frefactoring-compose-ui.md";
-export default new Map([["/src/images/blog/hadouken.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Frefactoring-compose-ui.md", __ASTRO_IMAGE_IMPORT_3iL3t]]);
-		
